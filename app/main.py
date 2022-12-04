2aa0e90bd613fffb24c1985361054b64e54b7f18
tjmoon0104 <tjmoon0104@outlook.com>
2022-12-04 22:32:01 +0100

    override 422 error to 400 and 405 error POST sentences check exists

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Path, HTTPException, status, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
-from pydantic import ValidationError
 from starlette.responses import JSONResponse
 
 from models import Sentence, SentenceWithCypher
@@ -43,16 +42,38 @@ sentence_dict = {
 }
 
 
-# Override 422 RequestValidationError to 405
+# Override 422 RequestValidationError to 400 and 405
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    for pydantic_error in exc.errors():
+        # Error Info
+        err_loc = pydantic_error.get('loc', '')
+        err_msg = pydantic_error.get('msg', '')
+        err_type = pydantic_error.get('type', '')
+
+        # GET sentence error
+        if {'path', 'sentence_id'}.issubset(err_loc):
+            if 'type_error' in err_type:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content=jsonable_encoder({"detail": f"Invalid ID supplied ({err_msg})"}),
+                )
+        # POST sentence error
+        elif {'body'}.issubset(err_loc):
+            if 'type_error' in err_type:
+                return JSONResponse(
+                    status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+                    content=jsonable_encoder({"detail": f"Invalid input ({err_msg})"}),
+                )
+    # Other errors
     return JSONResponse(
-        status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-        content=jsonable_encoder({"detail": "Invalid input"}),
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content=jsonable_encoder({"detail": "Unknown Error"}),
     )
 
 
-@app.get('/sentences/{sentence_id}', response_model=SentenceWithCypher, status_code=status.HTTP_200_OK)
+@app.get('/sentences/{sentence_id}',
+         response_model=SentenceWithCypher, status_code=status.HTTP_200_OK)
 def get_sentences_sentence_id(sentence_id: int = Path(
     default=None,
     description="ID of sentence to return",
@@ -62,11 +83,7 @@ def get_sentences_sentence_id(sentence_id: int = Path(
     """
     if sentence_id not in sentence_dict:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Sentence not found")
-    try:
-        encrypted_sentence = SentenceWithCypher(**sentence_dict[sentence_id])
-    except ValidationError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ID supplied")
-    return encrypted_sentence
+    return SentenceWithCypher(**sentence_dict[sentence_id])
 
 
 @app.post('/sentences/', response_model=SentenceWithCypher, status_code=status.HTTP_200_OK)
@@ -74,4 +91,10 @@ def post_sentences_(body: Sentence):
     """
     Add a new sentence to the store
     """
+    if body.id in sentence_dict:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Sentence ID exists")
+    else:
+        sentence_dict.update({
+            body.id: {"id": body.id, "text": body.text}
+        })
     return body
